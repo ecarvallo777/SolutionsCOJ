4e4bccd84fda2c2c4c8a68b86f0801d1e8482741
crewxart <crewxart@gmail.com>
2015-04-24 01:06:47 +0200

    Create 1000-ProblemA+B.java

---
+++ 1000-ProblemA+B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class suma{
+	public static void main(String [] args){
+		int a;
+		int b;
+		Scanner entrada= new Scanner(System.in);
+
+		a=entrada.nextInt();
+		b=entrada.nextInt();
+		System.out.print(a+b);
+	}
+}
