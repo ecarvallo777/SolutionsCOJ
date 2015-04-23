92bfebe95cd88acf036fa79318441436156f3b7f
crewxart <crewxart@gmail.com>
2015-04-24 01:17:17 +0200

    Create 1312-R2.java

---
+++ 1312-R2.java
@@ -0,0 +1,15 @@
+	import java.util.Scanner;
+
+public class rs {
+
+    public static void main(String[] args) {
+
+ 	Scanner entrada=new Scanner(System.in);
+	
+	int a= entrada.nextInt();
+
+	int b= entrada.nextInt();
+
+	System.out.print((b*2)-a);
+}	
+}
