2eb19ba3d85e5122adba038e5752d7cf55c4b9bc
crewxart <crewxart@gmail.com>
2015-04-24 01:18:12 +0200

    Create 1494-Dontread.java

---
+++ 1494-Dontread.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class first {
+
+    public static void main(String[] args) {
+  	Scanner entrada=new Scanner(System.in);
+	
+	entrada.nextLine();{
+	
+	System.out.print("I got my first solution!!!");
+		}
+}
+}
