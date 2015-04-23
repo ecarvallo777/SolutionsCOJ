f28ca3fb90a868c3f80d8ab2509b9cf3a510e22c
crewxart <crewxart@gmail.com>
2015-04-24 01:15:53 +0200

    Create 1293-PowOfTwo.java

---
+++ 1293-PowOfTwo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class dosa{
+	private static Scanner entrada;
+
+	public static void main(String [] args){
+
+
+
+
+
+		int N;
+	
+		int f=0;
+
+
+		Scanner in = new Scanner(System.in);
+		BigInteger b= new BigInteger("2");
+
+		N = in.nextInt();
+		
+
+			//if (0 <= N && N <= 265)
+		
+
+                 
+		
+
+
+		System.out.println(b.pow(N));
+
+	}
+}
